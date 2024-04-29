fe9dea955c080067e14d3262f1ebb774bf4b66a2
Mark Byrne <markbyrne8495@gmail.com>
2024-04-29 10:28:05 +0100

    finished basic python quiz

--- Python-Quiz/main.py
+++ Python-Quiz/main.py
@@ -14,4 +14,9 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've Completed the Quiz")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
