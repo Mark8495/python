3b99c2fbe54e2de897224187aefa95f6307492af
Mark Byrne <markbyrne8495@gmail.com>
2024-04-30 12:02:30 +0100

    turtle hurst painting

--- Python-Turtle/main.py
+++ Python-Turtle/main.py
@@ -1,15 +1,39 @@
-from turtle import Turtle, Screen
-
-timmy_the_turtle = Turtle()
-
+import turtle as turtle_module
+import random
 
 
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(22, 13, 33),
+(39, 48, 58),(173, 233, 255),
+(4, 38, 86),
+(72, 163, 31),
+(64, 156, 232),
+(232, 20, 200),
+(92, 39, 130),
+(74, 61, 89),
+(32, 33, 32)]
 
 
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
 
-
-
-
-screen = Screen()
+screen = turtle_module.Screen()
 screen.exitonclick()
